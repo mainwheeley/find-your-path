f7386e40aeb0c88f601d8e86d93236c190b448f6
jackielwu <jackielwu@gmail.com>
2017-11-11 22:38:01 -0500

    initial add of favroutes

---
+++ find-your-path/views/FavRoutes.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import { Component, View, Text, StyleSheet } from 'react-native';
+
+export default FavRoutes ({ navigation }) => (
+  <View>
+
+  </View>
+)
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#FFF',
+    alignItems: 'center',
+    justifyContent: 'center',
+  }
+})
