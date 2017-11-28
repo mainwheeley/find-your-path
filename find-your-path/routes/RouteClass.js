17e8e3d9a4804e0bb02f858438cd873fba4b9551
Revanth Nagurla <rnagurla@purdue.edu>
2017-11-27 21:22:59 -0500

    Added initial route class design and struct

---
+++ find-your-path/routes/RouteClass.js
@@ -0,0 +1,7 @@
+function RouteInfo(startingPoint, parkFlag, beachFlag, numMiles){
+  this.startingPoint = startingPoint;
+  this.count = 1;
+  this.parkFlag = parkFlag;
+  this.beachFlag = beachFlag;
+  this.numMiles = numMiles;
+}
