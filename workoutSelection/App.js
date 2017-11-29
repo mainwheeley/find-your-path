7e1e95e331c3a370fc4414da055920a6033912d1
Willy Tanggono <willy.tanggo@gmail.com>
2017-11-28 21:00:43 -0500

    finished selection for workout type

--- workoutSelection/App.js
+++ workoutSelection/App.js
@@ -1,13 +1,41 @@
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import RadioForm, {RadioButton, RadioButtonInput, RadioButtonLabel} from 'react-native-simple-radio-button';
+
+var radio_props = [
+  {label: 'Walking', value: 'walk' },
+  {label: 'Running', value: 'run' },
+  {label: 'Bicycle', value: 'bike' },
+  {label: 'Other', value: 'etc' },
+
+];
 
 export default class App extends React.Component {
+    constructor() {
+	super();
+	this.state = {
+	    value: 'walk'
+	};
+    }
+
+    logOnChange(valuex) {
+	this.setState({value:valuex});
+	console.log(valuex);
+    }
+
   render() {
     return (
       <View style={styles.container}>
-        <Text>Open up App.js to start working on your app!</Text>
-        <Text>Changes you make will automatically reload.</Text>
-        <Text>Shake your phone to open the developer menu.</Text>
+	<RadioForm
+	    radio_props={radio_props}
+	    initial={0}
+	    onPress={(value) => 
+		{this.logOnChange(value)}
+	    }
+	    buttonColor={'#8BC34A'}
+	    formHorizontal={true}
+	    labelHorizontal={false}
+	/>
       </View>
     );
   }
