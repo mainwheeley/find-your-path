cda6363ee2f6951d81ee3f7a20896e6d205b2508
Revanth Nagurla <rnagurla@purdue.edu>
2017-12-01 16:23:28 -0500

    Updated Workout goals and popular routes

--- find-your-path/views/comps/components/Stats.js
+++ find-your-path/views/comps/components/Stats.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { 
+import {
     AppRegistry,
     Button,
     StyleSheet,
@@ -65,7 +65,7 @@ export default class Stats extends React.Component {
 		    <Image style={styles.photo} resizeMode="contain" source={require('../imgs/fire.jpg')} />
 		</View>
 		<View style={styles.textWrap}>
-		    <Text style={styles.statsList}>Ave. cal.: 150 cals</Text>
+		    <Text style={styles.statsList}>Ave. cal burned: 150 cals</Text>
 		</View>
 	    </View>
 	</View>
@@ -80,7 +80,7 @@ export default class Stats extends React.Component {
 		    <Image style={styles.photo} resizeMode="contain" source={require('../imgs/time.png')} />
 		</View>
 		<View style={styles.textWrap}>
-		    <Text style={styles.statsList}>Total time run: 5 hrs</Text>
+		    <Text style={styles.statsList}>Total time run: 12 hrs</Text>
 		</View>
 	    </View>
 	</View>
@@ -89,16 +89,76 @@ export default class Stats extends React.Component {
 	    <View style={styles.lineSeparator}></View>
 	</View>
 
-	<View style={styles.statsRow}>
-	    <View style={styles.statsGrid}>
-		<View style={styles.photoWrap}>
-		    <Image style={styles.photo} resizeMode="contain" source={require('../imgs/star.png')} />
-		</View>
-		<View style={styles.textWrap}>
-		    <Text style={styles.statsList}>Fav route: 134 W. State St</Text>
-		</View>
-	    </View>
-	</View>
+  <View style={styles.statsRow}>
+      <View style={styles.statsGrid}>
+    <View style={styles.photoWrap}>
+        <Image style={styles.photo} resizeMode="contain" source={require('../imgs/star.png')} />
+    </View>
+    <View style={styles.textWrap}>
+        <Text style={styles.statsList}>My Popular Routes</Text>
+    </View>
+      </View>
+  </View>
+
+  <View style={styles.statsRow}>
+      <View style={styles.statsGrid}>
+    <View style={styles.photoWrap}>
+        <Image style={styles.photo} resizeMode="contain" source={require('../imgs/one.jpg')} />
+    </View>
+    <View style={styles.textWrap}>
+        <Text style={styles.statsListRoute}>134 W State St.</Text>
+        <Text style={styles.statsListDescription}>Total Distance: 6 mi</Text>
+        <Text style={styles.statsListDescription}># of times ran: 4</Text>
+        <Text style={styles.statsListDescription}>Specified Dest: Trails</Text>
+        <Text style={styles.statsListDescription}>Workout Type: Bike</Text>
+    </View>
+      </View>
+  </View>
+
+  <View style={styles.statsRow}>
+      <View style={styles.statsGrid}>
+    <View style={styles.photoWrap}>
+        <Image style={styles.photo} resizeMode="contain" source={require('../imgs/two.jpg')} />
+    </View>
+    <View style={styles.textWrap}>
+        <Text style={styles.statsListRoute}>610 Purdue Mall</Text>
+        <Text style={styles.statsListDescription}>Total Distance: 5 mi</Text>
+        <Text style={styles.statsListDescription}># of times ran: 3</Text>
+        <Text style={styles.statsListDescription}>Specified Dest: Beach</Text>
+        <Text style={styles.statsListDescription}>Workout Type: Run</Text>
+    </View>
+      </View>
+  </View>
+
+  <View style={styles.statsRow}>
+      <View style={styles.statsGrid}>
+    <View style={styles.photoWrap}>
+        <Image style={styles.photo} resizeMode="contain" source={require('../imgs/three.jpg')} />
+    </View>
+    <View style={styles.textWrap}>
+        <Text style={styles.statsListRoute}>230 S Grant St.</Text>
+        <Text style={styles.statsListDescription}>Total Distance: 4 mi</Text>
+        <Text style={styles.statsListDescription}># of times ran: 2</Text>
+        <Text style={styles.statsListDescription}>Specified Dest: Park</Text>
+        <Text style={styles.statsListDescription}>Workout Type: Walk</Text>
+    </View>
+      </View>
+  </View>
+
+  <View style={styles.statsRow}>
+      <View style={styles.lineSeparator}></View>
+  </View>
+
+  <View style={styles.statsRow}>
+      <View style={styles.statsGrid}>
+    <View style={styles.photoWrap}>
+        <Image style={styles.photo} resizeMode="contain" source={require('../imgs/heart.jpg')} />
+    </View>
+    <View style={styles.textWrap}>
+        <Text style={styles.statsList}>Fav. route: 134 W State St.</Text>
+    </View>
+      </View>
+  </View>
 
 	</View>
 	</ScrollView>
@@ -126,6 +186,16 @@ const styles = StyleSheet.create({
 	fontSize: 20,
 	fontWeight: 'bold',
 	textAlignVertical: 'center',
+    },
+    statsListRoute: {
+      fontSize: 18,
+      fontWeight: 'bold',
+      textAlignVertical: 'center',
+    },
+    statsListDescription: {
+      fontSize: 13,
+      fontWeight: 'bold',
+      textAlignVertical: 'center',
     },
     statsGrid: {
 	flex: 1,
@@ -156,4 +226,3 @@ const styles = StyleSheet.create({
 //	alignSelf: 'stretch',
     },
 });
-
