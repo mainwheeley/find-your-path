db60a2cfd1ccd13f026c77bcd42c18199816ef9d
aldiongudjiharto <aldio.ngudjiharto@gmail.com>
2018-01-10 13:32:08 -0500

    Update Statlist.js

--- profile-mockup/comps/components/Statlist.js
+++ profile-mockup/comps/components/Statlist.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { StyleSheet, Text, View, Button, Alert, FlatList, Image } from 'react-native';
 
+
 export default class App extends React.Component {
     render() {
     return (
